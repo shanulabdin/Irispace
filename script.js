a97a894f654aa1ef7c9e880a70b1048f3e042dfc
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-09-16 20:29:55 +0500

    change color and size of navbar

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('hello')
+const navbar = document.querySelector('.navbar');
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+  toggle.classList.toggle('active')
+})
